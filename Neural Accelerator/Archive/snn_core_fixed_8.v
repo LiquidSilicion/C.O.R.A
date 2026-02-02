800a5e346408e007ad29645b5333a902483128fd
LiquidSilicon <145273982+LiquidSilicion@users.noreply.github.com>
2026-02-02 16:20:17 +0530

    Update snn_core_fixed_8.v

--- Neural Accelerator/Archive/snn_core_fixed_8.v
+++ Neural Accelerator/Archive/snn_core_fixed_8.v
@@ -40,14 +40,16 @@ module snn_core_fixed8(
     '{8'd2, 8'd1, 8'd1, 8'd1, 8'd4, 8'd5, 8'd5, 8'd6}
 };
 parameter [9:0] W_OUT_SIMPLE [0:7] = '{
-    10'b1000_0000_00,  // Neuron 0 → Command 0
-    10'b0100_0000_00,  // Neuron 1 → Command 1  
-    10'b0010_0000_00,  // Neuron 2 → Command 2
-    10'b0001_0000_00,  // Neuron 3 → Command 3
-    10'b0000_1000_00,  // Neuron 4 → Command 4
-    10'b0000_0100_00,  // Neuron 5 → Command 5
-    10'b0000_0010_00,  // Neuron 6 → Command 6
-    10'b0000_0001_00   // Neuron 7 → Command 7
+    10'b1000000000,
+    10'b0100000000,
+    10'b0010000000,
+    10'b0001000000,
+    10'b0000100000,
+    10'b0000010000,
+    10'b0000001000,
+    10'b0000000100,
+    10'b0000000010,
+    10'b0000000001
 };
     endmodule
 );
