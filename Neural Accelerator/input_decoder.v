a2a630026cc5bd84974dc5eb14212d767ca0f0e8
LiquidSilicon <145273982+LiquidSilicion@users.noreply.github.com>
2026-02-04 05:00:52 -0500

    Update input_decoder.v

--- Neural Accelerator/input_decoder.v
+++ Neural Accelerator/input_decoder.v
@@ -1,1 +1,14 @@
+module input_from_aer();
+  input clk,
+  input rst_n,
+  input wire [31:0]in,
+  input aer_valid,
+  
+  
+  output reg [3:0]channel_Id,
+  output reg [23:0]timestamp;
+  
+  always@
 
+
+endmodule
