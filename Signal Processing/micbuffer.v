0f351f25bd7e3bb53422a9aa1a002af1c4213a04
Sherleine  Aliza Godinho <sherleineg@gmail.com>
2026-02-04 23:03:53 -0500

    Refactor fifo module for improved readability

--- Signal Processing/micbuffer.v
+++ Signal Processing/micbuffer.v
@@ -1,5 +1,3 @@
-// Code your testbench here
-// or browse Examples
 module fifo(
   input clk,
   input rst,
@@ -10,38 +8,36 @@ module fifo(
   output full,
   output empty
 );
-  //memory
-  reg[15:0] mem [127:0];
+
+  reg [15:0] mem [127:0];
   
-  //pointers
-  reg[6:0] wr_ptr;
-  reg[6:0] rd_ptr;
-  
-  //write operation
-  always@(posedge clk or negedge rst)
-    if(~rst)
-      begin
-        wr_ptr <=0;
-        else if(wr_en && !full)begin
-          mem[wr_ptr] <= din;
-          wr_ptr <= wr_ptr+1;
-        end
-      end
-  
-  //read operation
-  always@(posedge clk or negedge rst)begin
-    if(~rst)begin
-      rd_ptr <= 0;
-      dout <=0;
+  reg [6:0] wr_ptr;
+  reg [6:0] rd_ptr;
+
+  // Write
+  always @(posedge clk or negedge rst) begin
+    if (~rst) begin
+      wr_ptr <= 0;
+    end 
+    else if (wr_en && !full) begin
+      mem[wr_ptr] <= din;
+      wr_ptr <= wr_ptr + 1;
     end
-    else if (rd_en && !empty)
-      begin
-        dout <= mem[rd_ptr];
-        rd_ptr <= rd_ptr+1;
-      end
   end
-  
-  
+
+  // Read
+  always @(posedge clk or negedge rst) begin
+    if (~rst) begin
+      rd_ptr <= 0;
+      dout   <= 0;
+    end 
+    else if (rd_en && !empty) begin
+      dout   <= mem[rd_ptr];
+      rd_ptr <= rd_ptr + 1;
+    end
+  end
+
   assign empty = (wr_ptr == rd_ptr);
-  assign full = ((wr_ptr+1)== rd_ptr);
+  assign full  = ((wr_ptr + 1) == rd_ptr);
+
 endmodule
