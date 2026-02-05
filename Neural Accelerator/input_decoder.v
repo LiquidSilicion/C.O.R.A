393c74931d7de6c0d36c61f8ad051bd9f504df6e
LiquidSilicon <145273982+LiquidSilicion@users.noreply.github.com>
2026-02-04 23:44:40 -0500

    Update input_decoder.v

--- Neural Accelerator/input_decoder.v
+++ Neural Accelerator/input_decoder.v
@@ -1,32 +1,34 @@
-module input_from_aer();
+module input_from_aer(
   input clk,
   input rst_n,
-  input wire [23:0]in,
+  input wire [23:0] in,
   input aer_valid,
   
   output reg spike_detected,
-  output reg [3:0]channel_Id,
-  output reg [19:0]timestamp,
-  output reg timestamp_valid;
+  output reg [3:0] channel_Id,
+  output reg [19:0] timestamp,
+  output reg timestamp_valid
+);
   
-  always@(posedge clk or negedge rst_n)
-    if(!rst_n)
-      begin
-        channel_Id <= 4'b0;
-        timestamp <= 20'b0;
+  always@(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+      channel_Id <= 4'b0;
+      timestamp <= 20'b0;
+      spike_detected <= 1'b0;
+      timestamp_valid <= 1'b0;
+    end
+    else begin
+      if (aer_valid) begin
+        channel_Id <= in[23:20];
+        timestamp <= in[19:0];
+        spike_detected <= 1'b1;
+        timestamp_valid <= 1'b1;
+        $display("AER input is %d: %d", channel_Id, timestamp);
+      end
+      else begin
         spike_detected <= 1'b0;
         timestamp_valid <= 1'b0;
       end
-    else
-    begin
-      if (aer_valid)
-        begin
-    channel_id <= in[23:20];
- timestamp <= in[19:0];
-          spike_detected <= 1'b1;
-        timestamp_valid <= 1'b1;
-          $display("AER input is %d: %d,",channel_id[3:0], timestamp[19:0]);
-        end
-        end
+    end
   end
 endmodule
