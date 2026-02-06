6d58f9ed6f101ea7fbed67228df8680fc3fe08db
LiquidSilicon <145273982+LiquidSilicion@users.noreply.github.com>
2026-02-06 02:42:30 -0500

    Refactor aer_pipetb module to use data instead of in

--- Neural Accelerator/aer_input/aer_pipetb.v
+++ Neural Accelerator/aer_input/aer_pipetb.v
@@ -1,20 +1,19 @@
+module aer_pipetb();
+  reg clk;
+  reg rst_n;
+  reg [23:0]data;
+  reg aer_valid;
 
-module aer_pipetb(
-  reg clk,
-  reg rst_n,
-  reg [23:0]in,
-  reg aer_valid,
-
-  wire [3:0]channel_Id,
-  wire [19:0] timestamp,
-  wire timestamp_valid,
-  wire fifo_full,
-  wire fifo_empty);
+  wire [3:0]channel_Id;
+  wire [19:0] timestamp;
+  wire timestamp_valid;
+  wire fifo_full;
+  wire fifo_empty;
 
   aer_pipeline dut (
     .clk(clk),
     .rst_n(rst_n),
-    .in(in),
+    .data(data),
     .aer_valid(aer_valid),
     .channel_Id(channel_Id),
     .timestamp(timestamp),
@@ -22,6 +21,8 @@ module aer_pipetb(
     .fifo_full(fifo_full),
     .fifo_empty(fifo_empty)
   );
+  
+  integer i;
   
   initial begin
     clk = 1'b0;
@@ -33,20 +34,32 @@ module aer_pipetb(
   #10;
   rst_n = 1;
   #10;
-  in = 24'hA12345;
+  data = 24'hA12345;
   aer_valid = 1;
   #100;
   aer_valid = 0;
   #100;
   
+  
+  for (i=0;i<=24'hFFFFFF;i=i+1) begin
+  aer_valid = 1;
+  #10;
+  aer_valid = 0;
+  data <= i;
+  i = i + 24'h0000f0;
+  #10;
+  end
+  
+  
   rst_n = 1'b0;
   #10;
   rst_n = 1;
   #10;
-  in = 24'hA22245;
+  data = 24'hA22245;
   aer_valid = 1;
   #100;
   aer_valid = 0;
-  #100;
+  #100000000;
+  
   end
 endmodule
