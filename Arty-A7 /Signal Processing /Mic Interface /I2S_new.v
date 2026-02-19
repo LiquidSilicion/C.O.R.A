96e685828887b69a921c266b0e394deb184771cd
LiquidSilicon <145273982+LiquidSilicion@users.noreply.github.com>
2026-02-19 00:48:50 -0500

    Update I2S_new.v

--- Arty-A7 /Signal Processing /Mic Interface /I2S_new.v
+++ Arty-A7 /Signal Processing /Mic Interface /I2S_new.v
@@ -10,9 +10,9 @@ module i2s(
 );
 
     reg bclk_sync, bclk_prev;
-    reg lrclk_sync;
+    reg lrclk_sync, lrclk_prev;
     reg sd_sync;
-    reg [31:0] shift_reg;
+    reg [15:0] shift_reg;
     reg [4:0] bit_count;
     reg [15:0] left_chan, right_chan;
     
@@ -21,6 +21,7 @@ module i2s(
             bclk_sync <= 0;
             bclk_prev <= 0;
             lrclk_sync <= 0;
+            lrclk_prev <= 0;
             sd_sync <= 0;
             shift_reg <= 0;
             bit_count <= 0;
@@ -30,18 +31,21 @@ module i2s(
             bclk_sync <= bclk;
             bclk_prev <= bclk_sync;
             lrclk_sync <= lrclk;
+            lrclk_prev <= lrclk_sync;
             sd_sync <= sd;
             
             sample_valid <= 0;
             
-            if (bclk_sync && !bclk_prev) begin  // Rising edge
-                shift_reg <= {shift_reg[30:0], sd_sync};
+            if (lrclk_sync != lrclk_prev) begin
+                bit_count <= 0;
+            end
+            
+            if (bclk_sync && !bclk_prev) begin
+                shift_reg <= {shift_reg[14:0], sd_sync};
                 bit_count <= bit_count + 1;
                 
-                // After 16 bits, we have a complete sample
-                if (bit_count == 16) begin
-                    // Use left channel (or average both channels)
-                    audio_sample <= shift_reg[15:0];
+                if (bit_count == 15) begin
+                    audio_sample <= {shift_reg[14:0], sd_sync};
                     sample_valid <= 1'b1;
                     bit_count <= 0;
                 end
