5c2d4eeecd6fd3ae1f51ef457ea990a92cdc6079
LiquidSilicon <145273982+LiquidSilicion@users.noreply.github.com>
2026-02-25 05:35:55 -0500

    Create aer_depacket_fifo_dummy.v

---
+++ Pipeline - Top Module/Dummy Modules/aer_depacket_fifo_dummy.v
@@ -0,0 +1,16 @@
+module aer_depacket_fifo_dummy(
+    input wire clk,
+    input wire rst_n,
+    input wire data_valid,
+    input wire data_in,
+    output reg [14:0]data_out);
+    
+    always@(posedge clk or negedge rst_n)
+        if(!rst_n)begin
+            data_out<=0;
+        end else begin
+            if(data_valid)begin
+                data_out<= data_in;
+            end
+     end
+endmodule
