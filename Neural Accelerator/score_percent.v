3e3b3520cb197e884dab50fd1f7bbdcb152b3f32
LiquidSilicon <145273982+LiquidSilicion@users.noreply.github.com>
2026-04-20 10:51:02 +0530

    Create score_percent.v

---
+++ Neural Accelerator/score_percent.v
@@ -0,0 +1,23 @@
+module score_percent #(
+    parameter SCORE_WIDTH = 17,
+    parameter PERCENT_BITS = 7
+)(
+    input wire signed [SCORE_WIDTH-1:0] score_in,
+    output reg [PERCENT_BITS-1:0] percent_out
+);
+
+wire signed [SCORE_WIDTH+6:0] scaled;
+assign scaled = score_in * 100;
+
+always @(*) begin
+    if (score_in[SCORE_WIDTH-1]) begin
+        percent_out = {PERCENT_BITS{1'b0}};
+    end else begin
+        if (scaled[SCORE_WIDTH+6:8] > 100)
+            percent_out = 7'd100;
+        else
+            percent_out = scaled[SCORE_WIDTH+6:8];
+    end
+end
+
+endmodule
